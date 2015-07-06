c4960337f5e898ee8648d43bf1ac51ba4427f9df
xephos049 <joseph.joe93@gmail.com>
2015-07-05 21:25:21 -0700

    cleanup

--- PlaySoundsViewController.swift
+++ PlaySoundsViewController.swift
@@ -18,14 +18,6 @@ class PlaySoundsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
-//            var filePathUrl = NSURL.fileURLWithPath(filePath)
-//            
-//            
-//            audioPlayer.prepareToPlay()
-//        }else {
-//            println("Filepath is empty")
-//        }
         
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioPlayer.enableRate = true
